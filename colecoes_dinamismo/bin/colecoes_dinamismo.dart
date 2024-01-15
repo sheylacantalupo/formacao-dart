e2bbbc25f851356975621b53e1e18a1d67492a20
Sheyla Cantalupo <sheylacantalupo.sc@gmail.com>
2024-01-15 16:48:05 -0300

    switch

--- colecoes_dinamismo/bin/colecoes_dinamismo.dart
+++ colecoes_dinamismo/bin/colecoes_dinamismo.dart
@@ -2,7 +2,8 @@
 void main() {
 
   //escolherMeioTransporte(0);
-  escolherMeioTransporteEnum(Transporte.bike);
+  //escolherMeioTransporteEnum(Transporte.bike);
+  escolherMeioTransporteSwitch(Transporte.aviao);
 
 }
 
@@ -33,6 +34,34 @@ void escolherMeioTransporteEnum (Transporte locomocao){
     print("Vou de bike para aventura");
   }else{
     print("Não vou");
+  }
+
+}
+
+void escolherMeioTransporteSwitch(Transporte locomocao){
+
+  switch(locomocao){
+    case Transporte.carro:
+      print("Vou de carro para aventura");
+      break;
+    case Transporte.bike:
+      print("Vou de bike para aventura");
+      break;
+    case Transporte.aviao:
+      print("Vou de avião para aventura");
+      break;
+    case Transporte.andando:
+      print("Vou andando para aventura");
+      break; 
+    case Transporte.skate:
+      print("Vou de skate para aventura");
+      break;
+    case Transporte.patins:
+      print("Vou de patins para aventura");
+      break;
+    default:
+      print("Estou indo para a aventura");
+      
   }
 
 }
